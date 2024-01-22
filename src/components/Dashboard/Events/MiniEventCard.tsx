8063586efa1376e04ca89979f259baf272d51f07
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2024-01-22 15:37:29 -0800

    styles: apply max width to feed columns

--- src/components/Dashboard/Events/MiniEventCard.tsx
+++ src/components/Dashboard/Events/MiniEventCard.tsx
@@ -4,13 +4,13 @@ import React from 'react';
 
 const MiniEventCard = () => {
   return (
-    <div className='flex flex-row w-full items-center justify-center space-x-1 my-2 border-l-2 border-blue-600 hover:bg-gray-100 hover:cursor-pointer'>
+    <div className='flex lg:flex-row flex-wrap w-full items-center justify-center my-2 border-l-2 border-blue-600 hover:bg-gray-100 hover:cursor-pointer'>
       <div className='flex flex-col items-center -ml-2'>
         <p className='text-md text-gray-900 font-extrabold'>29</p>
         <p className='text-sm text-gray-900 font-semibold -mt-2'>Feb</p>
       </div>
       <div className='flex flex-col items-start'>
-        <div className='flex flex-row items-center justify-between'>
+        <div className='hidden lg:flex lg:flex-row items-center justify-between'>
           <h1 className='text-xs text-gray-800 font-medium ml-1'>
             Wednesday Practice
           </h1>
@@ -22,9 +22,12 @@ const MiniEventCard = () => {
           </Link>
         </div>
         <div className='flex flex-row items-center justify-between'>
-          <CalendarCheck className='h-4 w-4 text-blue-500 mx-1' />
-          <p className='text-xs text-gray-500'>
+          <CalendarCheck className='lg:block hidden h-4 w-4 text-blue-500 mx-1' />
+          <p className='lg:block hidden text-xs text-gray-500'>
             Thursday, Feb 29, 2024 at 6:00 PM
+          </p>
+          <p className='lg:hidden block text-xs text-gray-500 truncate ml-1'>
+            Wednesday Practice
           </p>
         </div>
       </div>
