167cbe9e22f4f776f762854a8901834459902782
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2024-02-12 09:09:28 -0800

    styles: add some style to event card

--- src/components/Dashboard/Events/EventCard.tsx
+++ src/components/Dashboard/Events/EventCard.tsx
@@ -1,5 +1,8 @@
 import { ExtendedEvent } from '@/app/dashboard/events/page';
+import { cn } from '@/lib/utils';
 import { User } from '@prisma/client';
+import { MapPin } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
 
 interface EventCardProps {
@@ -28,20 +31,31 @@ const EventCard = ({ event, user }: EventCardProps) => {
   const hasAnswered = event.participants.some((p) => p.userId === user.id);
 
   return (
-    <div className='flex flex-row items-center justify-start bg-white shadow-md rounded-md w-full mb-4 p-4 overflow-hidden h-[120px]'>
-      <div className='w-1/4 bg-gray-200 flex flex-col p-4 rounded-sm items-center justify-center'>
-        <h1 className='text-gray-900 font-medium text-3xl -m-2 p-0'>21</h1>
-        <p className='text-gray-500 text-lg'>June</p>
-      </div>
-      <div className='w-3/4 flex flex-col p-4'>
-        <h1 className='text-gray-900 font-medium text-lg'>event.name</h1>
-        <p className='text-gray-500 text-sm'>event.description</p>
-        <div className='flex flex-row justify-between'>
-          <p className='text-gray-500 text-sm'>event.location</p>
-          <p className='text-gray-500 text-sm'>event.time</p>
+    <Link href={`/events/${event.id}`}>
+      <div className='relative flex flex-row items-center justify-start bg-white shadow-md rounded-md w-full mb-4 p-4 overflow-hidden h-[120px]'>
+        <div className={cn(!hasAnswered ? 'bg-red-500' : 'bg-green-500', 'w-[2px] h-full absolute left-0 top-0 bottom-0')}></div>
+        <div className='w-1/4 bg-gray-200 flex flex-col p-4 rounded-sm items-center justify-center'>
+          <h1 className='text-green-700 font-medium text-3xl -m-2 p-0'>21</h1>
+          <p className='text-gray-900 text-lg font-medium'>June</p>
+        </div>
+        <div className='w-3/4 flex flex-col p-4'>
+          <h1 className='text-green-900 font-medium text-lg hover:underline'>
+            Monday Practice
+          </h1>
+          <p className='text-gray-700 text-sm'>Today at 5:30PM</p>
+          <div className='flex flex-row items-center justify-start'>
+            <MapPin className='h-4 w-4  text-green-900' />
+            <p className='text-gray-700 text-sm hover:text-green-900'>
+              2268 Seagull ct. San Jacinto, CA
+            </p>
+          </div>
+
+          <p className='text-gray-700 text-sm'>
+            Golden State Baseball &#x2022; 9U
+          </p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
