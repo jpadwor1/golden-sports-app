0fc09dd02df67dd41cbad7cdc80d3c8fc4785362
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2024-02-11 20:44:16 -0800

    styles: add some style to event card

--- src/components/Dashboard/Events/EventCard.tsx
+++ src/components/Dashboard/Events/EventCard.tsx
@@ -25,22 +25,24 @@ interface EventCardProps {
 }
 
 const EventCard = ({ event, user }: EventCardProps) => {
+  const hasAnswered = event.participants.some((p) => p.userId === user.id);
+
   return (
-  <div className='flex flex-row bg-white shadow-md rounded-md w-full mb-4 p-4 overflow-hidden'>
-    <div className="w-1/4 bg-red-300 flex flex-col p-4">
-      <h1 className="text-gray-900 font-medium text-xl">21</h1>
-      <p className="text-gray-500 text-sm">June</p>
+    <div className='flex flex-row items-center justify-start bg-white shadow-md rounded-md w-full mb-4 p-4 overflow-hidden h-[120px]'>
+      <div className='w-1/4 bg-gray-200 flex flex-col p-4 rounded-sm items-center justify-center'>
+        <h1 className='text-gray-900 font-medium text-3xl -m-2 p-0'>21</h1>
+        <p className='text-gray-500 text-lg'>June</p>
+      </div>
+      <div className='w-3/4 flex flex-col p-4'>
+        <h1 className='text-gray-900 font-medium text-lg'>event.name</h1>
+        <p className='text-gray-500 text-sm'>event.description</p>
+        <div className='flex flex-row justify-between'>
+          <p className='text-gray-500 text-sm'>event.location</p>
+          <p className='text-gray-500 text-sm'>event.time</p>
+        </div>
+      </div>
     </div>
-    <div className="w-3/4 flex flex-col p-4 m-4">
-      <h1 className="text-gray-900 font-medium text-lg">event.name</h1>
-      <p className="text-gray-500 text-sm">event.description</p>
-      <div className="flex flex-row justify-between">
-        <p className="text-gray-500 text-sm">event.location</p>
-        <p className="text-gray-500 text-sm">event.time</p>
-      </div>
-      </div>
-  </div>
-  )
+  );
 };
 
 export default EventCard;
