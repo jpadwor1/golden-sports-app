b983403b72f3c4359a6f4b08678d81aa699d6a72
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2024-04-15 14:30:41 -0700

    refactor: switch to cockroach db

--- src/components/Dashboard/Events/EventCard.tsx
+++ src/components/Dashboard/Events/EventCard.tsx
@@ -32,17 +32,17 @@ const EventCard = ({ event, user }: EventCardProps) => {
 
   return (
     <Link href={`/events/${event.id}`}>
-      <div className='relative flex flex-row items-center justify-start bg-white shadow-md rounded-md w-full mb-4 p-4 overflow-hidden h-[120px]'>
+      <div className='relative flex flex-row items-center justify-start bg-white shadow-md rounded-md w-full mb-4 p-2 overflow-hidden h-[120px]'>
         <div className={cn(!hasAnswered ? 'bg-red-500' : 'bg-green-500', 'w-[2px] h-full absolute left-0 top-0 bottom-0')}></div>
         <div className='w-1/4 bg-gray-200 flex flex-col p-4 rounded-sm items-center justify-center'>
           <h1 className='text-green-700 font-semibold text-3xl -m-2 p-0'>21</h1>
           <p className='text-gray-900 text-lg font-medium'>June</p>
         </div>
-        <div className='w-3/4 flex flex-col p-4'>
+        <div className='w-3/4 flex flex-col ml-2 leading-6'>
           <h1 className='text-green-900 font-medium text-lg hover:underline'>
             Monday Practice
           </h1>
-          <p className='text-gray-700 text-sm'>Today at 5:30PM</p>
+          <p className='text-gray-900 text-md'>Today at 5:30PM</p>
           <div className='flex flex-row items-center justify-start'>
             <MapPin className='h-4 w-4  text-green-900' />
             <p className='text-gray-700 text-sm hover:text-green-900'>
