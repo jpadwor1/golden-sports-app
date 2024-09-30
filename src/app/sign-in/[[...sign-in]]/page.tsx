f17b7cbd1d0926009608b511e5f206279eb2c283
John Padworski <128070765+jpadwor1@users.noreply.github.com>
2024-09-29 19:51:21 -0700

    add signin and signout pages

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,13 @@
 import Footer from "@/components/Navigation/Footer";
 import Navbar from "@/components/Navigation/Navbar";
-import { SignUp } from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <>
       <Navbar />
       <div className="flex items-center justify-center min-h-[calc(100dvh-30rem)]">
-        <SignUp />
+        <SignIn />
       </div>
       <Footer />
     </>
